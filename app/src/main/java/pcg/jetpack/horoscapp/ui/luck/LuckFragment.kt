44883f527b9c11017080dc8adc8db04dc8f9922e
PCarretero28 <120803089+PCarretero28@users.noreply.github.com>
2024-08-22 13:54:41 +0200

    initial commit

---
+++ app/src/main/java/pcg/jetpack/horoscapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,23 @@
+package pcg.jetpack.horoscapp.ui.luck
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import pcg.jetpack.horoscapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/pcg/jetpack/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,26 @@
+package pcg.jetpack.horoscapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pcg.jetpack.horoscapp.R
+import pcg.jetpack.horoscapp.databinding.FragmentHoroscopeBinding
+import pcg.jetpack.horoscapp.databinding.FragmentLuckBinding
+import pcg.jetpack.horoscapp.databinding.FragmentPalmistryBinding
+
+class PalmistryFragment : Fragment() {
+
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
