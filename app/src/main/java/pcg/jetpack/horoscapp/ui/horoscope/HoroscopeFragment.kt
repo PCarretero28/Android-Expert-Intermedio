bcce504499e31e698994e6dc6d1ff88220f62626
PCarretero28 <120803089+PCarretero28@users.noreply.github.com>
2024-09-23 22:28:57 +0200

    initUI fragment

--- app/src/main/java/pcg/jetpack/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/pcg/jetpack/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -17,6 +17,20 @@ class HoroscopeFragment : Fragment() {
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
--- app/src/main/java/pcg/jetpack/horoscapp/ui/horoscope/HoroscopeViewModel.kt
+++ app/src/main/java/pcg/jetpack/horoscapp/ui/horoscope/HoroscopeViewModel.kt
@@ -2,9 +2,24 @@ package pcg.jetpack.horoscapp.ui.horoscope
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import pcg.jetpack.horoscapp.domain.model.HoroscopeInfo
+import pcg.jetpack.horoscapp.domain.model.HoroscopeInfo.*
 import javax.inject.Inject
 
 @HiltViewModel
 class HoroscopeViewModel @Inject constructor(): ViewModel() {
+
+    private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
+    val horoscope:StateFlow<List<HoroscopeInfo>> = _horoscope
+
+
+
+    init {
+        _horoscope.value = listOf(
+            Aries, Gemini, Pisces, Capricorn, Sagittarius
+        )
+    }
 
 }
